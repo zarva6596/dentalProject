1d8297277c9956379b558193e4d90a383b007a97
zarva6596 <zarva6596@gmail.com>
2024-12-10 17:54:10 +0200

    STYLE: add general style settings to tailwind config and connect the project fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,95 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: 'GeneralSans'
+    },
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: '#1376f8',
+          dark: '#011632',
+          sky: '#25b4f8',
+          light: '#e6f6fe'
+        },
+        white: '#fff',
+        text: '#3c4959',
+        success: '#17bf28',
+        warnint: '#ec942c',
+        error: '#e52323'
+      },
+      fontSize: {
+        'h1': [
+          '3.875rem',
+          {
+            lineHeight: '1.2',
+            letterSpacing: '-2%',
+            fontWeight: '600',
+          }
+        ],
+        'h2': [
+          '2.5rem',
+          {
+            lineHeight: '1.25',
+            letterSpacing: '0',
+            fontWeight: '600',
+          }
+        ],
+        'h3': [
+          '2.25rem',
+          {
+            lineHeight: '1.25',
+            letterSpacing: '0',
+            fontWeight: '600',
+          }
+        ],
+        'h4': [
+          '2.25rem',
+          {
+            lineHeight: '1.2',
+            letterSpacing: '0',
+            fontWeight: '600',
+          }
+        ],
+
+        b1: [
+          '1.125rem',
+          {
+            lineHeight: '1.55',
+            letterSpacing: '1.5%',
+            fontWeight: '400',
+          }
+        ],
+
+        b2: [
+          '1rem',
+          {
+            lineHeight: '1.55',
+            letterSpacing: '1.5%',
+            fontWeight: '400',
+          }
+        ],
+
+
+        b3: [
+          '0.875rem',
+          {
+            lineHeight: '1.55',
+            letterSpacing: '2%',
+            fontWeight: '400',
+          }
+        ],
+
+        b4: [
+          '0.75rem',
+          {
+            lineHeight: '1.55',
+            letterSpacing: '1.5%',
+            fontWeight: '400',
+          }
+        ],
+      }
+    },
   },
   plugins: [],
 }
