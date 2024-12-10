115c9a574de4922eafa751624198b13580f71197
zarva6596 <zarva6596@gmail.com>
2024-12-10 20:03:58 +0200

    feat: add user dropdown component and put it to the header

--- src/components/general/DHeader.vue
+++ src/components/general/DHeader.vue
@@ -4,14 +4,14 @@
 
 <template>
     <header class="max-w-header w-full mx-auto px-10 py-4.5 bg-blue-light rounded-lg my-10 flex atems-center justify-between">
-        <nuxt-link to="/">
+        <nuxt-link to="/" class="flex items-center">
             <img src="~/assets/images/logos/logo.svg" alt="Logo image">
         </nuxt-link>
 
         <DNavBar />
 
         <div>
-            Auth Bar
+            <DUserDropdown />
         </div>
     </header>
 </template>
