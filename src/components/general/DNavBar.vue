cab9f589ad3a3f126eff5a5b65318d0dec363b54
zarva6596 <zarva6596@gmail.com>
2024-12-10 19:40:41 +0200

    fix: remove error with upload the right route and mark active item

--- src/components/general/DNavBar.vue
+++ src/components/general/DNavBar.vue
@@ -1,27 +1,33 @@
 <script setup lang="ts">
+const route = useRoute()
+
 const navItems = ref([
     {
-        active: true,
+        active: false,
         id: '1',
         title: 'Home',
         route: '/',
     },
     {
+        active: false,
         id: '2',
         title: 'Services',
         route: '/services',
     },
     {
+        active: false,
         id: '3',
         title: 'Blogs',
         route: '/blogs',
     },
     {
+        active: false,
         id: '4',
         title: 'About',
         route: '/about',
     },
     {
+        active: false,
         id: '5',
         title: 'Contacts',
         route: '/contacts',
@@ -31,10 +37,21 @@ const navItems = ref([
 function changeRoute(id: string) {
     navItems.value = navItems.value.map(item => ({ ...item, active: item.id === id }))
 }
+
+onMounted(() => {
+    const id = navItems.value.find(item => item.route === route.path)?.id
+
+    id && changeRoute(id)
+})
+
+const navBarActive = computed(() => navItems.value.some(item => item.active))
 </script>
 
 <template>
-    <nav class="flex items-center text-blue-dark">
+    <nav
+        class="flex items-center text-blue-dark transition-all duration-100"
+        :class="navBarActive ? 'opacity-100' : 'opacity-0'"    
+    >
         <ul class="flex items-center gap-x-9">
             <li
                 v-for="item in navItems"
