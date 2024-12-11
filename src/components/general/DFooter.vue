c6d7b4ffff03c67182b42cb18673dd66f52d2231
zarva6596 <zarva6596@gmail.com>
2024-12-11 15:30:35 +0200

    fix: add some line to the footer

--- src/components/general/DFooter.vue
+++ src/components/general/DFooter.vue
@@ -23,6 +23,10 @@ const { openMobileMenu } = useMobileMenuStore()
             </div>
         </div>
 
+        <div class="mx-5 text-b1 font-medium lg:hidden mb-8 text-text">
+            All our PRO level features at your fingertips.
+        </div>
+
         <div class="lg:hidden mx-5 text-b1 font-semibold mb-2.5">Follow us on</div>
 
         <div
