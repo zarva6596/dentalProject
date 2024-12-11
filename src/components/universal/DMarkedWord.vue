2a66f241c1e04c12681be1b4fdb223089a4c95f0
zarva6596 <zarva6596@gmail.com>
2024-12-11 16:51:40 +0200

    feat: create the component for the marked words

---
+++ src/components/universal/DMarkedWord.vue
@@ -0,0 +1,13 @@
+<template>
+    <span class="marked-word relative">
+        <slot />
+
+        <span class="inline-block absolute bottom-0 right-0 h-1 w-[105%]">
+            <img
+                src="~/assets/images/underline.png"
+                alt="Underline image"
+                class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full"
+            />
+        </span>
+    </span>
+</template>
