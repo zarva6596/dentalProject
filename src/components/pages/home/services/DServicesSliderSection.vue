a54c5755bae9a9f63c4ec4197e9eb6f37c55ee7a
zarva6596 <zarva6596@gmail.com>
2024-12-12 18:19:31 +0200

    chore: create the services section component

---
+++ src/components/pages/home/services/DServicesSliderSection.vue
@@ -0,0 +1,5 @@
+<template>
+    <DSection class="bg-blue-light py-12 px-5 mb-40">
+        Card
+    </DSection>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import DFirstScreen from '~/components/pages/home/firstScreen/DFirstScreen.vue'
+import DServicesSliderSection from '~/components/pages/home/services/DServicesSliderSection.vue';
 </script>
 
 <template>
     <div>
         <DFirstScreen />
+
+        <DServicesSliderSection />
     </div>
 </template>
