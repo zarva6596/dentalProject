fc4cb1c9f4c98b25980cf32a1daaa83437eeac10
zarva6596 <zarva6596@gmail.com>
2024-12-13 02:06:05 +0200

    feat: create the benefits section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import DBenefitsSection from '~/components/pages/home/benefits/DBenefitsSection.vue';
 import DBookSection from '~/components/pages/home/book/DBookSection.vue';
 import DFirstScreen from '~/components/pages/home/firstScreen/DFirstScreen.vue'
 import DPhoneFormSection from '~/components/pages/home/phoneForm/DPhoneFormSection.vue';
@@ -12,6 +13,8 @@ import DServicesSliderSection from '~/components/pages/home/services/DServicesSl
         <DServicesSliderSection />
 
         <DPhoneFormSection />
+
+        <DBenefitsSection />
 
         <DBookSection />
     </div>
