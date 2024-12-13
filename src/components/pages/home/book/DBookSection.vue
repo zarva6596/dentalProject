14b9340e30b1b6c0dff9464bddea0f9e573e9648
zarva6596 <zarva6596@gmail.com>
2024-12-13 02:05:21 +0200

    chore: change headings and descriptions on the sections to the universal components

--- src/components/pages/home/book/DBookSection.vue
+++ src/components/pages/home/book/DBookSection.vue
@@ -1,17 +1,17 @@
 <template>
     <DSection class="pt-24 pb-17.5 flex flex-col gap-12 lg:flex-row lg:items-end lg:gap-40 lg:!px-17.5">
         <div>
-            <h2 class="text-h3 lg:text-h2 mb-4 lg:mb-5">
+            <DSectionHeading>
                 Leave your worries at the door and enjoy a 
                 <DMarkedWord class="lg:hidden">healthier</DMarkedWord>
                 <span class="hidden lg:inline-block">healthier</span>, more 
                 <DMarkedWord class="hidden lg:inline-block">precise smile</DMarkedWord>
                 <span class="lg:hidden">precise smile</span>
-            </h2>
+            </DSectionHeading>
 
-            <p class="text-b1 mb-7 lg:mb-10">
+            <DDescriptionWrapper>
                 We use only the best quality materials on the market in order to provide the best products to our patients, So don’t worry about anything and book yourself.
-            </p>
+            </DDescriptionWrapper>
 
             <DButton>Book an appointment</DButton>
         </div>
--- src/components/pages/home/phoneForm/DPhoneFormSection.vue
+++ src/components/pages/home/phoneForm/DPhoneFormSection.vue
@@ -1,13 +1,13 @@
 <template>
     <DSection class="pt-24 pb-20 flex flex-col gap-12 lg:flex-row lg:items-end lg:gap-40 lg:!px-17.5">
         <div>
-            <h2 class="max-w-[500px] text-h3 lg:text-h2 mb-4 lg:mb-5">
+            <DSectionHeading class="max-w-[500px]">
                 We’re <DMarkedWord>welcoming</DMarkedWord> new patients and can’t wait to meet you.
-            </h2>
+            </DSectionHeading>
 
-            <p class="text-b1 mb-7 lg:mb-10">
+            <DDescriptionWrapper>
                 We use only the best quality materials on the market in order to provide the best products to our patients, So don’t worry about anything and book yourself.
-            </p>
+            </DDescriptionWrapper>
 
             <DPhoneForm />
         </div>
