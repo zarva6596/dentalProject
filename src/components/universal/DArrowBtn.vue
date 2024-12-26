9c5153059cecad7b31d2e0332bd36e57aaf70dd0
zarva6596 <zarva6596@gmail.com>
2024-12-26 23:08:01 +0200

    style: add color fix

--- src/components/universal/DArrowBtn.vue
+++ src/components/universal/DArrowBtn.vue
@@ -7,21 +7,21 @@ withDefaults(defineProps<{ darkMode?: boolean, type?: 'prev' | 'next' }>(), {
 
 <template>
     <button
-        class="rounded-0 border-none py-2.5 px-6"
-        :class="darkMode ? 'bg-black' : 'bg-white'"
+        class="rounded-0 border-none py-2.5 px-6 hover:opacity-70 transition-all duration-500"
+        :class="darkMode ? 'bg-blue-dark' : 'bg-white'"
     >
         <img
             v-if="type === 'prev'"
             src="~/assets/images/icons/arrowLeft.svg"
             alt="Left arrow icon"
-            :class="darkMode && 'invert'"
+            :class="darkMode && 'invert grayscale'"
         />
 
         <img
             v-else
             src="~/assets/images/icons/arrowRight.svg"
             alt="Right arrow icon"
-            :class="darkMode && 'invert'"
+            :class="darkMode && 'invert grayscale'"
         />
     </button>
 </template>
