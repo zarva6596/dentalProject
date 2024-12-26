59f8e5dc456a013947be0104811ff3d0f6417b6c
zarva6596 <zarva6596@gmail.com>
2024-12-26 22:26:15 +0200

    feat: create the happy clients section

---
+++ src/components/pages/home/happyClients/DHappyClientsSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <DSection class="pt-25 pb-16 lg:pb-12">
+        <div class="flex flex-col lg:items-center lg:text-center">
+            <DSectionHeading>
+                Our <DMarkedWord>Happy Clients</DMarkedWord>
+            </DSectionHeading>
+
+            <DDescriptionWrapper class="max-w-[480px]">
+                We use only the best quality materials on the market in order to provide the best products to our patients.
+            </DDescriptionWrapper>
+        </div>
+
+        <div>
+            slider
+        </div>
+
+        <DSliderArrows dark-mode />
+    </DSection>
+</template>
