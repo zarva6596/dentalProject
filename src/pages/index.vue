5d461d56b0d4813c244a06140d47932268cc539d
zarva6596 <zarva6596@gmail.com>
2024-12-26 21:08:08 +0200

    chore: show the Meet section on the home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,7 @@
 import DBenefitsSection from '~/components/pages/home/benefits/DBenefitsSection.vue';
 import DBookSection from '~/components/pages/home/book/DBookSection.vue';
 import DFirstScreen from '~/components/pages/home/firstScreen/DFirstScreen.vue'
+import DMeetSection from '~/components/pages/home/meet/DMeetSection.vue';
 import DPhoneFormSection from '~/components/pages/home/phoneForm/DPhoneFormSection.vue';
 import DPlaylistPreview from '~/components/pages/home/playlistPreview/DPlaylistPreview.vue';
 import DServicesSliderSection from '~/components/pages/home/services/DServicesSliderSection.vue';
@@ -20,5 +21,7 @@ import DServicesSliderSection from '~/components/pages/home/services/DServicesSl
         <DBookSection />
 
         <DPlaylistPreview />
+
+        <DMeetSection />
     </div>
 </template>
