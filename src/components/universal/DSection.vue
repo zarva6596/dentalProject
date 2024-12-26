80cdb96de7f5cd3366abad3aaf797e2b78924a92
zarva6596 <zarva6596@gmail.com>
2024-12-26 20:56:53 +0200

    fix: add props for full width on a section

--- src/components/universal/DSection.vue
+++ src/components/universal/DSection.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+    fullWidth?: boolean
+    background?: boolean
+}>(), {
+    fullWidth: false,
+    background: false
+})
+</script>
+
 <template>
-    <section class="px-5 max-w-content mx-auto">
+    <section
+        class="px-5 max-w-content mx-auto"
+        :class="fullWidth && 'relative'"
+    >
+        <div
+            v-if="fullWidth"
+            class="-z-10 pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-full w-screen"
+            :class="background && 'bg-blue-light'"    
+        />
+
         <slot />
     </section>
 </template>
