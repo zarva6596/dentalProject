c26847d10fae7a95d04e86f27a8bc1c04aea3aab
zarva6596 <zarva6596@gmail.com>
2024-12-26 16:18:02 +0200

    feat: create the Playlist preview section

---
+++ src/components/pages/home/playlistPreview/DPlaylistPreview.vue
@@ -0,0 +1,25 @@
+<template>
+    <DSection class="flex flex-col items-center">
+        <div class="flex flex-col items-center lg:text-center">
+            <DSectionHeading class="max-w-[630px] capitalize">
+                We’re <DMarkedWord>Welcoming</DMarkedWord> new patients and can’t wait to meet you.
+            </DSectionHeading>
+
+            <DDescriptionWrapper class="max-w-[460px]">
+                We use only the best quality materials on the market in order to provide the best products to our patients.
+
+                <span class="lg:hidden">So don’t worry about anything and book yourself.</span>
+            </DDescriptionWrapper>
+        </div>
+
+        <div class="max-w-[1064px] aspect-square lg:aspect-3/1.2 rounded-md overflow-hidden mb-12 lg:mb-10">
+            <img
+                src="~/assets/images/pages/home/videoSection.png"
+                alt="Video preview image"
+                class="h-full w-full object-cover object-center"    
+            />
+        </div>
+
+        <DButton>Watch Playlist</DButton>
+    </DSection>
+</template>
