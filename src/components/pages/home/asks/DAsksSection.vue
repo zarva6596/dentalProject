de8b68210e005a2c1e444c58376ca2b7da78551a
zarva6596 <zarva6596@gmail.com>
2024-12-27 21:35:11 +0200

    feat: create asks section

---
+++ src/components/pages/home/asks/DAsksSection.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import DAccordeon from '~/components/universal/accordeon/DAccordeon.vue';
+import type { AccordeonItem } from '~/types/accordeon';
+
+const items: AccordeonItem[] = [
+    {
+        title: 'Can I see who reads my email campaigns?',
+        description: 'Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.'
+    },
+    {
+        title: 'Do you offer non-profit discounts?',
+        description: 'Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.'
+    },
+    {
+        title: 'Can I see who reads my email campaigns?',
+        description: 'Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.'
+    },
+    {
+        title: 'Can I see who reads my email campaigns?',
+        description: 'Lorem ipsum dolor sit amet consectetur. Convallis cras placerat dignissim aliquam massa. Aliquet volutpat rhoncus in convallis consectetur. Cras adipiscing volutpat non hac enim odio enim.'
+    },
+]
+</script>
+
+<template>
+    <DSection class="pt-16 lg:pt-12 pb-30">
+        <div class="lg:text-center flex flex-col lg:items-center mb-2.5 lg:mb-6">
+            <DSectionHeading>
+                <DMarkedWord>
+                    Frequently Ask Question
+                </DMarkedWord>
+            </DSectionHeading>
+
+            <DDescriptionWrapper class="max-w-[450px] !text-b2">
+                We use only the best quality materials on the market in order to provide the best products to our patients.
+            </DDescriptionWrapper>
+        </div>
+
+        <div class="flex justify-center">
+            <DAccordeon :items />
+        </div>
+    </DSection>
+</template>
