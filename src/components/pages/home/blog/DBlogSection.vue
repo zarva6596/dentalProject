3a4f9ca91471590b09c0ef872f45b4668e5447de
zarva6596 <zarva6596@gmail.com>
2024-12-27 12:51:39 +0200

    feat: create the section vith news and articles for the home page

---
+++ src/components/pages/home/blog/DBlogSection.vue
@@ -0,0 +1,32 @@
+<template>
+    <DSection class="py-16 lg:py-12">
+        <div class="flex items-end justify-between">
+            <div class="max-w-[455px]">
+                <DSectionHeading>
+                    <DMarkedWord>
+                        News & Articles
+                    </DMarkedWord>
+                </DSectionHeading>
+
+                <DDescriptionWrapper class="!mb-0">
+                    We use only the best quality materials on the market in order to provide the best products to our patients.
+                </DDescriptionWrapper>
+            </div>
+
+            <DButton class="hidden lg:block">View All</DButton>
+        </div>
+
+        <div class="mt-12 mb-10 lg:mt-16 lg:mb-12">
+            cards
+        </div>
+
+        <div class="lg:flex justify-center hidden">
+            <DButton>Check out more</DButton>
+        </div>
+
+        <DSliderArrows
+            dark-mode
+            class="lg:hidden"
+        />
+    </DSection>
+</template>
