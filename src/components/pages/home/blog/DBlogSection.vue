d82826aed4ab160719d427dbc1d2105bbabab5b9
zarva6596 <zarva6596@gmail.com>
2024-12-27 13:49:50 +0200

    chore: show posts items on the home page

--- src/components/pages/home/blog/DBlogSection.vue
+++ src/components/pages/home/blog/DBlogSection.vue
@@ -1,3 +1,67 @@
+<script setup lang="ts">
+import type { PostItem } from '~/types/blog';
+import DBlogItemCard from './DBlogItemCard.vue';
+
+const items: PostItem[] = [
+    {
+        photo: './pages/home/posts/post1.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+    {
+        photo: './pages/home/posts/post2.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+    {
+        photo: './pages/home/posts/post3.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+    {
+        photo: './pages/home/posts/post4.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+    {
+        photo: './pages/home/posts/post5.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+    {
+        photo: './pages/home/posts/post6.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+    {
+        photo: './pages/home/posts/post7.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+    {
+        photo: './pages/home/posts/post8.png',
+        title: 'Care of your Teeth',
+        description: 'Lorem ipsum dolor sit amet consectetur.',
+        route: '/',
+        author: 'Anita Jackson'
+    },
+]
+</script>
+
 <template>
     <DSection class="py-16 lg:py-12">
         <div class="flex items-end justify-between">
@@ -16,8 +80,13 @@
             <DButton class="hidden lg:block">View All</DButton>
         </div>
 
-        <div class="mt-12 mb-10 lg:mt-16 lg:mb-12">
-            cards
+        <div class="mt-12 mb-10 lg:mt-16 lg:mb-12 lg:grid grid-cols-4 gap-5">
+            <DBlogItemCard
+                v-for="(item, i) in items"
+                :key="i"
+                v-bind="item"
+                :class="i && 'hidden lg:block'"
+            />
         </div>
 
         <div class="lg:flex justify-center hidden">
