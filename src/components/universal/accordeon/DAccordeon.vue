ba44f27c074472feb36c0fc55de73aad38092cc7
zarva6596 <zarva6596@gmail.com>
2024-12-27 21:34:50 +0200

    feat: create components for the acordeon and accordeon's item

---
+++ src/components/universal/accordeon/DAccordeon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { AccordeonItem } from '~/types/accordeon';
+import DAccordeonItem from './DAccordeonItem.vue';
+
+defineProps<{
+    items: AccordeonItem[]
+}>()
+
+const activeId = ref<number | null>(0)
+
+function chooseItem(i: number) {
+    activeId.value = activeId.value === i ? null : i
+}
+</script>
+
+<template>
+    <div class="max-w-[630px] flex flex-col gap-5">
+        <DAccordeonItem
+            v-for="(item, i) in items"
+            :key="i"
+            v-bind="item"
+            :active="activeId === i"
+            @open="chooseItem(i)"
+        />
+    </div>
+</template>
