d5b1660a0c64aece64a5e4b6bff20948b0defff3
zarva6596 <zarva6596@gmail.com>
2025-02-18 00:26:18 +0200

    chore: add other already implemented sections to the service page

--- src/pages/services.vue
+++ src/pages/services.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import DContactForm from '~/components/pages/services/DContactForm.vue';
 import DServicesMain from '~/components/pages/services/DServicesMain.vue';
+import DPlaylistPreview from '~/components/pages/home/playlistPreview/DPlaylistPreview.vue';
+import DMeetSection from '~/components/pages/home/meet/DMeetSection.vue';
+import DAskToActionSection from '~/components/pages/home/askToAction/DAskToActionSection.vue';
+import DAsksSection from '~/components/pages/home/asks/DAsksSection.vue';
 </script>
 
 <template>
@@ -8,5 +12,13 @@ import DServicesMain from '~/components/pages/services/DServicesMain.vue';
         <DServicesMain />
 
         <DContactForm />
+
+        <DPlaylistPreview class="!py-25" />
+
+        <DMeetSection />
+
+        <DAsksSection />
+
+        <DAskToActionSection />
     </div>
 </template>
