4b557d9dbfbdefbc4798602bccb4c36042d1eea8
zarva6596 <zarva6596@gmail.com>
2025-02-17 15:09:22 +0200

    chore: move services items from component to store

--- src/components/pages/services/DServicesMain.vue
+++ src/components/pages/services/DServicesMain.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import DServiceCard from '../home/services/DServiceCard.vue';
+import DServiceCard from '~/components/pages/home/services/DServiceCard.vue';
+import { useServiceStore } from '~/store/useServicesStore.js'
 
-
+const { items } = storeToRefs(useServiceStore())
 </script>
 
 <template>
-    <DSection class="pt-10">
-        <div class="flex flex-col items-center">
+    <DSection class="pt-10 pb-25 relative">
+        <div
+            class="bg-gradient-to-b from-blue-light to-white absolute w-screen h-[120vh] bottom-0 left-1/2 -translate-x-1/2"
+        />
+        
+        <div class="flex flex-col items-center relative">
             <DSectionHeading>
                 <DMarkedWord>
                     Services
@@ -18,7 +23,7 @@ import DServiceCard from '../home/services/DServiceCard.vue';
             </DDescriptionWrapper>
         </div>
 
-        <div>
+        <div class="grid grid-cols-3 gap-x-14 gap-y-12 relative">
             <DServiceCard
                 v-for="(item, i) in items"
                 :key="i"
