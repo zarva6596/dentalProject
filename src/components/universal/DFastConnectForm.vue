c8d418b92fcd4243a4f2426f4f66adef9e68ce25
zarva6596 <zarva6596@gmail.com>
2025-02-18 00:10:07 +0200

    style: add the loading component to the contact form

--- src/components/universal/DFastConnectForm.vue
+++ src/components/universal/DFastConnectForm.vue
@@ -1,36 +1,51 @@
 <script setup lang="ts">
 import { useToggle } from '@vueuse/core'
+import DLoading from './DLoading.vue';
 
 const [isLoading, toggleLoading] = useToggle(false)
 
 function submitForm() {
     toggleLoading(true)
 
-    setTimeout(() => toggleLoading(false), 3000)
+    setTimeout(() => {
+        Object.keys(formModel).forEach(key => formModel[key as keyof typeof formModel] = '')
+        toggleLoading(false)
+    }, 3000)
 }
+
+const formModel = reactive({
+    name: '',
+    phone: '',
+    email: ''
+})
 </script>
 
 <template>
-    <div class="bg-white w-full px-7 py-8 rounded-xl">
+    <div class="bg-white w-full px-7 py-8 rounded-xl relative overflow-hidden">
+        <DLoading :active="isLoading" />
+
         <form @submit.prevent="submitForm">
-            <h3 class="text-h4 capitalize font-semibold text-center">
+            <h3 class="text-h4 capitalize font-semibold text-center relative">
                 Request Appointment
             </h3>
 
             <div class="mt-10 mb-7 flex flex-col gap-5">
                 <input
+                    v-model="formModel.name"
                     placeholder="Full Name"
                     type="text"
                     class="border border-gray rounded-md px-5 py-3.5 placeholder:text-b3 w-full"
                 >
 
                 <input
+                    v-model="formModel.phone"
                     placeholder="Phone Number"
                     type="text"
                     class="border border-gray rounded-md px-5 py-3.5 placeholder:text-b3 w-full"
                 >
 
                 <input
+                    v-model="formModel.email"
                     placeholder="Email Address"
                     type="text"
                     class="border border-gray rounded-md px-5 py-3.5 placeholder:text-b3 w-full"    
