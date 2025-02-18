a5c6c3d9a18b22cda500d6342b2eceef45b61964
zarva6596 <zarva6596@gmail.com>
2025-02-18 20:42:52 +0200

    feat: implement main section for the about us page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import DAboutUs from '~/components/pages/about-us/DAboutUs.vue';
+
+</script>
+
 <template>
     <div>
-        About
+        <DAboutUs />
     </div>
 </template>
