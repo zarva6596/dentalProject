74e73f8265d2cb829f9dfb734b175cd82076b79f
zarva6596 <zarva6596@gmail.com>
2025-02-19 10:47:49 +0200

    chore: add the "Our Doctors" component on the "About Us" page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import DAboutUs from '~/components/pages/about-us/DAboutUs.vue';
+import DOurDoctors from '~/components/pages/about-us/DOurDoctors.vue'
 
 </script>
 
 <template>
     <div>
         <DAboutUs />
+
+        <DOurDoctors />
     </div>
 </template>
