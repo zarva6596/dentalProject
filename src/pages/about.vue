3b28bd0b921993422923712e893ccc91e5d9dd55
zarva6596 <zarva6596@gmail.com>
2025-02-19 14:51:07 +0200

    chore: add already done sections to the "about us" page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,6 +2,8 @@
 import DAboutUs from '~/components/pages/about-us/DAboutUs.vue';
 import DOurDoctors from '~/components/pages/about-us/DOurDoctors.vue'
 import DRandomInfo from '~/components/pages/about-us/DRandomInfo.vue';
+import DPlaylistPreview from '~/components/pages/home/playlistPreview/DPlaylistPreview.vue';
+import DServicesSliderSection from '~/components/pages/home/services/DServicesSliderSection.vue';
 </script>
 
 <template>
@@ -11,5 +13,9 @@ import DRandomInfo from '~/components/pages/about-us/DRandomInfo.vue';
         <DOurDoctors />
 
         <DRandomInfo />
+
+        <DServicesSliderSection class="mb-25 !bg-blue-dark" />
+
+        <DPlaylistPreview />
     </div>
 </template>
